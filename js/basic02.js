90b0736db3de03d99896029e74b42f23f50fe6ce
lee <sipossa@naver.com>
2022-12-27 11:51:04 +0900

    20221227

---
+++ js/basic02.js
@@ -0,0 +1,11 @@
+$(function () {
+    $('.main_slide').slick({
+        arrows: false,
+        autoplay: true,
+    });
+
+    $('.product_slide').slick({
+        arrows: false,
+        slidesToShow: 5,
+    })
+})
