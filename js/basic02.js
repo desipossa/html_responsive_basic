eb15ec9b5d2fc6d6d7fb9c96c74f695a1622861c
lee <sipossa@naver.com>
2022-12-27 15:00:27 +0900

    20221227

--- js/basic02.js
+++ js/basic02.js
@@ -1,4 +1,23 @@
 $(function () {
+
+    $('#header .gnb>ul>li>a').on('click', function (e) {
+        if ($(window).width() < 769) {
+            e.preventDefault();
+            //$(this).parent().siblings().find('.submenu').slideUp();
+            $(this).next().stop().slideToggle();
+            $(this).toggleClass('on')
+        }
+    });
+
+    $('#header .right').on('click', function () {
+        $('#header .gnb').toggleClass('on');
+    })
+
+    $(window).on('resize', function () {
+        $('.submenu').removeAttr('style');
+        $('#header .gnb>ul>li>a').removeClass('on');
+    })
+
     $('.main_slide').slick({
         arrows: false,
         //autoplay: true,
